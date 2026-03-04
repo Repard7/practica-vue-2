a851d8094c7dcc3faaf6cc6469a6400593da7af0
Ростислав Грачев <gracevrostislav5@gmail.com>
2026-03-04 12:49:05 +0700

    add base components

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+Vue.component('product-notes', {
+    data() {
+        return {
+            firstColumn: [],
+            secondColumn: [],
+            thirdColumn: [],
+        }
+    },
+    template: `
+
+    `,
+})
+
+Vue.component('card', {
+    props: {
+        cardId: {
+            type: [String, Number],
+            required: true
+        },
+
+        card: {
+            type: Object,
+            required: true
+        },
+
+        columnIndex: {
+            type: Number,
+            required: true
+        },
+
+        cardIndex: {
+            type: Number,
+            required: true
+        },
+
+        fulledSecondColumn: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+
+    `
+})
 
 let app = new Vue({
     el: '#app',
